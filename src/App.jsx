3b15c493da5b510be4a3e4f6c5947f7b3e3c2dde
kiqueangulo <kiqueangulo@gmail.com>
2022-12-22 00:05:51 -0500

    Simple code to test on ssr

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+import "./App.css"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const Increment = () => setCount(count + 1)
+
+  const Decrement = () => setCount(count - 1)
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+
+      <div className="controllers">
+        <button onClick={() => Increment()}>Increment</button>
+        <button onClick={() => Decrement()}>Decrement</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
